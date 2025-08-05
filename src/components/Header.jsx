a2661e99844eab55788a228807c80e093d9bd0c4
Ani-Peju <pjanimas099@gmail.com>
2025-08-05 17:10:39 -0400

    Updated the components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <header>
-            <img src="kassie.png" alt="Logo" className="items-left" />
+            <img src="" alt="Logo" className="items-left" />
         </header>
     );
 };
