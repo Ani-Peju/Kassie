3eb7a92d0b809272bd004b282d42b4ed5ca14544
Hammerarm2003 <davidadejolaoluwa@gmail.com>
2025-08-12 16:26:38 -0400

    Multi pages
    Multipages

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className=" bg-[#eef4ff]">
+      <Header />
+      <h1 className="text-center text-2xl mt-10">Contact info</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,36 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className=" bg-[#eef4ff] flex items-center justify-start text-left ml-20">
-      <img src="kassie.png" alt="Logo" className="h-10 mb-20 mt-6 ml-20" />
+    <header className=" bg-[#eef4ff] flex flex-row items-center justify-start text-left ml-20 items-align-center">
+      <img
+        src="kassie.png"
+        alt="Logo"
+        className="h-10 mb-20 mt-6 ml-20 mr-20"
+      />
+      <nav className="mr-10">
+        <ul className="flex space-x-6">
+          <li>
+            <a href="#home" className="text-gray-600 hover:text-gray-800">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="text-gray-600 hover:text-gray-800">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="text-gray-600 hover:text-gray-800">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="text-gray-600 hover:text-gray-800">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
